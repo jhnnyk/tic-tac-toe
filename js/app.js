0b7ffb095035e2504000f28775c9bf269013dbbd
John Knotts <jhnnyk@gmail.com>
2017-01-05 11:27:20 -0700

    display start screen at start

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var startScreen = document.getElementById('start');
+var finishScreen = document.getElementById('finish');
+var board = document.getElementById('board');
+
+// Set up initial display
+startScreen.style.display = "initial";
+board.style.display = "none";
+finishScreen.style.display = "none";
